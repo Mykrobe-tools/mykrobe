9f5b7f4184a87da23592f14901c5585b223aca78
Phelim Bradley <phelimb@hh-yoda-05-15.ebi.ac.uk>
2018-05-18 16:02:53 +0100

    Fix custom variant_to_resistance_json arg passing and bump version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def read(*names, **kwargs):
 
 setup(
     name='mykrobe',
-    version='0.6.5',
+    version='0.6.6',
     license='MIT',
     description='Mykrobe atlas',
     # long_description='%s\n%s' % (
