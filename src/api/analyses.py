17a39c6e3259fc7e2f7b0db2515cde894a477303
Phelim Bradley <wave@phel.im>
2018-07-05 09:47:00 +0100

    DEBUG logging

--- src/api/analyses.py
+++ src/api/analyses.py
@@ -37,6 +37,15 @@ from mykrobe.version import __version__
 import json
 import subprocess
 import requests
+import logging
+import http.client as http_client
+http_client.HTTPConnection.debuglevel = 1
+
+logging.basicConfig()
+logging.getLogger().setLevel(logging.DEBUG)
+requests_log = logging.getLogger("requests.packages.urllib3")
+requests_log.setLevel(logging.DEBUG)
+requests_log.propagate = True
 
 def load_json(f):
     with open(f, 'r') as infile:
