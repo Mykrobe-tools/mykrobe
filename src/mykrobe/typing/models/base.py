4cf437203586fe5044445d43a0f94b972b410fe8
Phelim Bradley <wave@phel.im>
2018-09-05 10:52:16 +0100

    Pass allele kmer length to kmer model to calculated expected kmer counts

--- src/mykrobe/typing/models/base.py
+++ src/mykrobe/typing/models/base.py
@@ -5,11 +5,12 @@ class ProbeCoverage(object):
 
     "Summary of kmer coverage of sequence. e.g output of color covearges"
 
-    def __init__(self, percent_coverage, median_depth, min_depth, k_count):
+    def __init__(self, percent_coverage, median_depth, min_depth, k_count, klen):
         self.percent_coverage = percent_coverage
         self.median_depth = median_depth
         self.min_depth = min_depth
         self.k_count = k_count
+        self.klen = klen
 
     @property
     def coverage_dict(self):
@@ -17,6 +18,7 @@ class ProbeCoverage(object):
                 "median_depth": round(self.median_depth, 2),
                 "min_non_zero_depth": round(self.min_depth, 2),
                 "kmer_count": self.k_count,
+                "klen": self.klen,
                 }
 
     def __str__(self):
