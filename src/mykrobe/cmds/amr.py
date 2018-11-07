0aaa5619e2bf4b370e0af61006e46e6db0295f15
martinghunt <martin.g.hunt@gmail.com>
2018-11-07 15:35:35 +0000

    Remove debug k count logging

--- src/mykrobe/cmds/amr.py
+++ src/mykrobe/cmds/amr.py
@@ -95,7 +95,6 @@ class ConfThresholder:
             # Check what allele_length means in depth_to_expected_kmer_coun()! Probably need to change next two lines...
             correct_k_count = (self.kmer_length * correct_covg[i]) + 0.01 # see KmerCountGenotypeModel.depth_to_expected_kmer_count()
             incorrect_k_count = (self.kmer_length * incorrect_covg[i]) + 0.01 # see KmerCountGenotypeModel.depth_to_expected_kmer_count()
-            logging.debug('correct_k_count ' + str(correct_k_count) + '. incorrect_k_count ' + str(incorrect_k_count))
 
             #correct_percent_coverage = ConfThresholder._simulate_percent_coverage(int(correct_k_count), 2 + self.kmer_length)
             #incorrect_percent_coverage = ConfThresholder._simulate_percent_coverage(int(incorrect_k_count), 2 + self.kmer_length)
