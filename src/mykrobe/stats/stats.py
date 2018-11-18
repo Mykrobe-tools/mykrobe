350aaf530b82306b2bb88b1b51fa3657951c14aa
Phelim Bradley <wave@phel.im>
2018-11-18 12:50:25 +0000

    Added special case in log_lik_probability_of_N_gaps for very high coverage samples exceeding floating point limits

--- src/mykrobe/stats/stats.py
+++ src/mykrobe/stats/stats.py
@@ -15,7 +15,14 @@ def log_lik_probability_of_N_gaps(depth, percent_coverage, L):
     percent_coverage = float(percent_coverage)/100
     n_gaps = int(round(L-(L*percent_coverage)))
     expected_n_gaps = exp(-depth) * L
-    return log_poisson_prob(expected_n_gaps, n_gaps)
+    try:
+        log_like= log_poisson_prob(expected_n_gaps, n_gaps)
+    except ValueError:
+        ## For very high coverage samples, expected_n_gaps goes to 0.0 breaking math.log
+        if expected_n_gaps<=0:
+            expected_n_gaps=1e-308        
+        log_like= log_poisson_prob(expected_n_gaps, n_gaps)
+    return log_like
 
 
 def log_poisson_prob(lam, k):
