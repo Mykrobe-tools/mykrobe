e540751773e0419bdf52a55479eebed80f621efa
Martin Hunt <martin.g.hunt@gmail.com>
2019-12-12 14:34:37 +0000

    fix path, hopefully

--- ci/windows_before_install.sh
+++ ci/windows_before_install.sh
@@ -1,4 +1,4 @@
-export PATH=/c/tools/cygwin/bin:$PATH
+export PATH=/cygdrive/c/tools/cygwin/bin:$PATH
 original_dir=$PWD
 cd mccortex/libs/htslib/
 git checkout 1832d3a1b75133e55fb6abffc3f50f8a6ed5ceae
