d1d6993abf2e0f67a484dd397cce70845e50adad
Martin Hunt <martin.g.hunt@gmail.com>
2019-12-13 15:43:42 +0000

    trying to get appveyor to work

--- ci/windows_xxhsum_fix.py
+++ ci/windows_xxhsum_fix.py
@@ -34,7 +34,7 @@ fix_file(filename, to_replace)
 
 filename = os.path.join("mccortex", "libs", "seq_file", "Makefile")
 to_replace = {
-    "LINKING=$(HTSARGS) -lpthread -lz\n": "LINKING=$(HTSARGS) -lpthread -lws2_32 -lz -llzma -lbz2\n",
+    "LINKING=$(HTSARGS) -lpthread -lz\n": "LINKING=$(HTSARGS) -lpthread -lws2_32 -lhts -lz -llzma -lbz2\n",
 }
 fix_file(filename, to_replace)
 
