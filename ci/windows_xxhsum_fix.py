ba308c8c273252dc3935b843ce35fc160e71a7bc
Martin Hunt <martin.g.hunt@gmail.com>
2019-12-13 14:19:49 +0000

    trying to get appveyor to work

--- ci/windows_xxhsum_fix.py
+++ ci/windows_xxhsum_fix.py
@@ -1,37 +1,40 @@
 #!/usr/bin/env python3
 
 import os
+
+
+def fix_file(filename, to_replace):
+    lines = []
+    with open(filename) as f:
+        for line in f:
+            if line in to_replace:
+                print(f"Replacing in {filename}:\n", line, to_replace[line], sep="")
+                line = to_replace[line]
+            lines.append(line)
+
+    with open(filename, "w") as f:
+        print(*lines, sep="", file=f)
+
+
 filename = os.path.join("mccortex", "libs", "xxHash", "xxhsum.c")
-lines = []
-to_replace = "#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(_WIN32) || defined(__CYGWIN__)\n"
-replace_with = "#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(_WIN32)\n"
-
-with open(filename) as f:
-    for line in f:
-        if line == to_replace:
-            line = replace_with
-        lines.append(line)
-
-
-with open(filename, "w") as f:
-    print(*lines, sep="", file=f)
+to_replace = {
+    "#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(_WIN32) || defined(__CYGWIN__)\n": "#if defined(MSDOS) || defined(OS2) || defined(WIN32) || defined(_WIN32)\n"
+}
+fix_file(filename, to_replace)
 
 
 
 filename = os.path.join("mccortex", "libs", "bit_array", "bit_array.c")
-lines = []
 to_replace = {
-        "  c = (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n": "  return (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n",
-        "static word_t __inline windows_popcount(word_t w)\n": "static word_t __inline windows_popcountl(word_t w)\n",
+    "  c = (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n": "  return (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n",
+    "static word_t __inline windows_popcount(word_t w)\n": "static word_t __inline windows_popcountl(word_t w)\n",
 }
-
-with open(filename) as f:
-    for line in f:
-        if line in to_replace:
-            print("Replacing", line)
-            line = to_replace[line]
-        lines.append(line)
+fix_file(filename, to_replace)
 
 
-with open(filename, "w") as f:
-    print(*lines, sep="", file=f)
+filename = os.path.join("mccortex", "libs", "seq_file", "Makefile")
+to_replace = {
+    "LINKING=$(HTSARGS) -lpthread -lz": "LINKING=$(HTSARGS) -lpthread -lz -llzma -lbz2",
+}
+fix_file(filename, to_replace)
+
