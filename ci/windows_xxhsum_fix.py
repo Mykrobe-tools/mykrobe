2789466769fae3ce52f4cf26c486048ae1b577ff
Martin Hunt <martin.g.hunt@gmail.com>
2019-12-13 22:07:28 +0000

    trying to get appveyor to work

--- ci/windows_xxhsum_fix.py
+++ ci/windows_xxhsum_fix.py
@@ -24,43 +24,43 @@ fix_file(filename, to_replace)
 
 
 
-filename = os.path.join("mccortex", "libs", "bit_array", "bit_array.c")
-to_replace = {
-    "  c = (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n": "  return (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n",
-    "static word_t __inline windows_popcount(word_t w)\n": "static word_t __inline windows_popcountl(word_t w)\n",
-}
-fix_file(filename, to_replace)
-filename = os.path.join("mccortex", "libs", "seq-align", "libs", "bit_array", "bit_array.c")
-fix_file(filename, to_replace)
-
-
-filename = os.path.join("mccortex", "libs", "seq_file", "Makefile")
-to_replace = {
-    "LINKING=$(HTSARGS) -lpthread -lz\n": "LINKING=$(HTSARGS) -lpthread -lws2_32 -lhts -lz -llzma -lcurl -lssl -lcrypto -lbz2\n",
-}
-fix_file(filename, to_replace)
-
-filename = os.path.join("mccortex", "libs", "seq_file", "benchmarks", "Makefile")
-to_replace = {
-    "LINKING=$(HTSARGS) $(SAMLINK) -lpthread -lz\n": "LINKING=$(HTSARGS) $(SAMLINK) -lpthread -lz -lws2_32 -lhts -lz -llzma -lcurl -lssl -lcrypto -lbz2\n",
-}
-fix_file(filename, to_replace)
-
-
-filename = os.path.join("mccortex", "libs", "seq_file", "dev", "Makefile")
-to_replace = {
-        "LINKING=$(HTSARGS) $(SAMLINK) -lpthread -lz\n": "LINKING=$(HTSARGS) $(SAMLINK) -lpthread -lz -lws2_32 -lhts -lz -llzma -lcurl -lssl -lcrypto -lbz2\n",
-}
-fix_file(filename, to_replace)
-
-
-to_replace = {
-    "#include <stdlib.h>\n": "#include <stdlib.h>\n#include <stdint.h>\n",
-}
-filenames = [
-  os.path.join("mccortex", "libs", "seq_file", "seq_file.h"),
-  os.path.join("mccortex", "libs", "seq-align", "libs", "seq_file", "seq_file.h"),
-  os.path.join("mccortex", "libs", "readsim", "seq_file.h"),
-]
-for filename in filenames:
-    fix_file(filename, to_replace)
+#filename = os.path.join("mccortex", "libs", "bit_array", "bit_array.c")
+#to_replace = {
+#    "  c = (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n": "  return (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n",
+#    "static word_t __inline windows_popcount(word_t w)\n": "static word_t __inline windows_popcountl(word_t w)\n",
+#}
+#fix_file(filename, to_replace)
+#filename = os.path.join("mccortex", "libs", "seq-align", "libs", "bit_array", "bit_array.c")
+#fix_file(filename, to_replace)
+#
+#
+#filename = os.path.join("mccortex", "libs", "seq_file", "Makefile")
+#to_replace = {
+#    "LINKING=$(HTSARGS) -lpthread -lz\n": "LINKING=$(HTSARGS) -lpthread -lws2_32 -lhts -lz -llzma -lcurl -lssl -lcrypto -lbz2\n",
+#}
+#fix_file(filename, to_replace)
+#
+#filename = os.path.join("mccortex", "libs", "seq_file", "benchmarks", "Makefile")
+#to_replace = {
+#    "LINKING=$(HTSARGS) $(SAMLINK) -lpthread -lz\n": "LINKING=$(HTSARGS) $(SAMLINK) -lpthread -lz -lws2_32 -lhts -lz -llzma -lcurl -lssl -lcrypto -lbz2\n",
+#}
+#fix_file(filename, to_replace)
+#
+#
+#filename = os.path.join("mccortex", "libs", "seq_file", "dev", "Makefile")
+#to_replace = {
+#        "LINKING=$(HTSARGS) $(SAMLINK) -lpthread -lz\n": "LINKING=$(HTSARGS) $(SAMLINK) -lpthread -lz -lws2_32 -lhts -lz -llzma -lcurl -lssl -lcrypto -lbz2\n",
+#}
+#fix_file(filename, to_replace)
+#
+#
+#to_replace = {
+#    "#include <stdlib.h>\n": "#include <stdlib.h>\n#include <stdint.h>\n",
+#}
+#filenames = [
+#  os.path.join("mccortex", "libs", "seq_file", "seq_file.h"),
+#  os.path.join("mccortex", "libs", "seq-align", "libs", "seq_file", "seq_file.h"),
+#  os.path.join("mccortex", "libs", "readsim", "seq_file.h"),
+#]
+#for filename in filenames:
+#    fix_file(filename, to_replace)
