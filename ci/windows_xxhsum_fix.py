1ccf6feea4599e387ccbc5583cb23430fd8558e8
Martin Hunt <martin.g.hunt@gmail.com>
2019-12-13 12:09:34 +0000

    trying to get appveyor to work

--- ci/windows_xxhsum_fix.py
+++ ci/windows_xxhsum_fix.py
@@ -15,3 +15,20 @@ with open(filename) as f:
 
 with open(filename, "w") as f:
     print(*lines, sep="", file=f)
+
+
+
+filename = os.path.join("mccortex", "libs", "bit_array", "bit_array.c")
+lines = []
+to_replace = "  c = (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n"
+replace_with = "  return (word_t)(w * ((word_t)~(word_t)0/255)) >> (sizeof(word_t) - 1) * 8;\n"
+
+with open(filename) as f:
+    for line in f:
+        if line == to_replace:
+            line = replace_with
+        lines.append(line)
+
+
+with open(filename, "w") as f:
+    print(*lines, sep="", file=f)
