caa0dbf7d8534df737d8a63c08648e88e52ff5b5
leoisl <leandro.ishi.lima@gmail.com>
2019-12-16 15:40:00 +0000

    Update windows_xxhsum_fix.py

--- ci/windows_xxhsum_fix.py
+++ ci/windows_xxhsum_fix.py
@@ -26,7 +26,7 @@ fix_file(filename, to_replace)
 filename = os.path.join("mccortex", "libs", "bcftools", "Makefile")
 to_replace = {
    "-rdynamic": "",
-   "-ldl": ""
+   "-ldl": "../htslib/libhts.a"
    # "\t$(CC) -rdynamic $(LDFLAGS) -o $@ $(OBJS) $(HTSLIB) -lpthread -lz -lm -ldl $(GSL_LIBS) $(LIBS)\n": "\t$(CC) $(LDFLAGS) -o $@ $(OBJS) $(HTSLIB) -lpthread -lz -lm $(GSL_LIBS) $(LIBS)\n",
 }
 fix_file(filename, to_replace)
