48b8f01502a83e6295c22cd8e17c00eaf8948609
Martin Hunt <martin.g.hunt@gmail.com>
2020-10-15 15:03:27 +0000

    Version bump 0.9.0

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ def read(*names, **kwargs):
 
 setup(
     name='mykrobe',
-    version='0.8.2',
+    version='0.9.0',
     license='MIT',
     description='Antibiotic resistance prediction in minutes',
     # long_description='%s\n%s' % (
