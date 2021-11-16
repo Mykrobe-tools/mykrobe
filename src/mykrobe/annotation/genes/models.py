a7846473a1c9de07921d2bf9805dd2112288d092
Michael Hall <michael@mbh.sh>
2021-11-16 14:47:59 +1000

    do not include stop codons with X amino acids

--- src/mykrobe/annotation/genes/models.py
+++ src/mykrobe/annotation/genes/models.py
@@ -1,12 +1,16 @@
 from __future__ import print_function
 
-from Bio import SeqIO
-from Bio.Seq import Seq
-from Bio.Data import CodonTable
 import itertools
-from mykrobe.utils import split_var_name
 import logging
 from collections import defaultdict
+
+from Bio import SeqIO
+from Bio.Data import CodonTable
+from Bio.Seq import Seq
+
+from mykrobe import STOP
+from mykrobe.utils import split_var_name
+
 logger = logging.getLogger(__name__)
 logger.setLevel("INFO")
 
@@ -16,7 +20,6 @@ def flatten(l):
 
 
 class Region(object):
-
     def __init__(self, reference, start, end, forward=True):
         self.reference = reference
         self.start = start
@@ -33,9 +36,9 @@ class Region(object):
     @property
     def seq(self):
         if self.forward:
-            return self.reference[self.start - 1:self.end]
+            return self.reference[self.start - 1 : self.end]
         else:
-            return self.reference[self.start - 1:self.end].reverse_complement()
+            return self.reference[self.start - 1 : self.end].reverse_complement()
 
     def get_reference_position(self, pos):
         if pos < 0 and self.forward:
@@ -52,7 +55,6 @@ class Region(object):
 
 
 class Gene(Region):
-
     def __init__(self, name, reference, start, end, forward=True):
         super(self.__class__, self).__init__(reference, start, end, forward)
         self.name = name
@@ -64,15 +66,15 @@ class Gene(Region):
         return self.seq.translate(table=self.translation_table).rstrip("*")
 
     def get_context(self, pos, N):
-        return self.seq[(3 * (pos - 1)) - N: pos * 3 + N]
+        return self.seq[(3 * (pos - 1)) - N : pos * 3 + N]
 
     def get_codon(self, pos):
         if pos > len(self.prot):
             raise ValueError(
-                "There are only %s aminoacids in this gene" % len(
-                    self.prot))
+                "There are only %s aminoacids in this gene" % len(self.prot)
+            )
         else:
-            return self.seq[(3 * (pos - 1)): pos * 3]
+            return self.seq[(3 * (pos - 1)) : pos * 3]
 
     def get_reference_codon(self, pos):
         if self.forward:
@@ -90,8 +92,7 @@ class Gene(Region):
         if self.forward:
             return condons_in_reading_frame
         else:
-            return [str(Seq(s).reverse_complement())
-                    for s in condons_in_reading_frame]
+            return [str(Seq(s).reverse_complement()) for s in condons_in_reading_frame]
 
     def __str__(self):
         return "Gene:%s" % self.name
@@ -116,12 +117,12 @@ def make_backward_codon_table():
 
 
 def generate_all_possible_codons():
-    return ["".join(subset)
-            for subset in itertools.product(["A", "T", "C", "G"], repeat=3)]
+    return [
+        "".join(subset) for subset in itertools.product(["A", "T", "C", "G"], repeat=3)
+    ]
 
 
-class GeneAminoAcidChangeToDNAVariants():
-
+class GeneAminoAcidChangeToDNAVariants:
     def __init__(self, reference, genbank):
         self.reference = self._parse_reference(reference)
         self.genbank = self._parse_genbank(genbank)
@@ -133,13 +134,13 @@ class GeneAminoAcidChangeToDNAVariants():
 
     def _parse_genbank(self, genbank):
         d = {}
-        with open(genbank, 'r') as infile:
+        with open(genbank, "r") as infile:
             for feat in SeqIO.read(infile, "genbank").features:
                 if feat.type == "gene":
                     try:
                         name = feat.qualifiers.get(
-                            "gene",
-                            feat.qualifiers.get("db_xref"))[0]
+                            "gene", feat.qualifiers.get("db_xref")
+                        )[0]
                     except TypeError:
                         pass
                     else:
@@ -156,12 +157,17 @@ class GeneAminoAcidChangeToDNAVariants():
                             self.reference,
                             start=feat.location.start + 1,
                             end=feat.location.end,
-                            forward=forward)
+                            forward=forward,
+                        )
         return d
 
     def get_alts(self, amino_acid):
         if amino_acid == "X":
-            return flatten(self.backward_codon_table.values())
+            # exclude stop codons
+            non_stop_codons = [
+                v for k, v in self.backward_codon_table.items() if k != STOP
+            ]
+            return flatten(non_stop_codons)
         else:
             return self.backward_codon_table[amino_acid]
 
@@ -169,14 +175,13 @@ class GeneAminoAcidChangeToDNAVariants():
         if gene.forward:
             return self.get_alts(amino_acid)
         else:
-            return [str(Seq(s).reverse_complement())
-                    for s in self.get_alts(amino_acid)]
+            return [str(Seq(s).reverse_complement()) for s in self.get_alts(amino_acid)]
 
     def get_location(self, gene, pos):
         if gene.forward:
             dna_pos = (3 * (pos - 1)) + 1
         else:
-            dna_pos = (3 * (pos))
+            dna_pos = 3 * (pos)
         return gene.get_reference_position(dna_pos)
 
     def get_variant_names(self, gene, mutation, protein_coding_var=True):
@@ -188,7 +193,8 @@ class GeneAminoAcidChangeToDNAVariants():
             return self._process_coding_mutation(gene, ref, start, alt)
         else:
             raise ValueError(
-                "Variants are defined in 1-based coordinates. You can't have pos 0. ")
+                "Variants are defined in 1-based coordinates. You can't have pos 0. "
+            )
 
     def _process_DNA_mutation(self, gene, ref, start, alt):
         names = []
@@ -196,7 +202,7 @@ class GeneAminoAcidChangeToDNAVariants():
         if not gene.forward:
             pos -= len(ref) - 1
             ref = str(Seq(ref).reverse_complement())
-            if alt != 'X':
+            if alt != "X":
                 alt = str(Seq(alt).reverse_complement())
         if alt == "X":
             for a in ["A", "T", "C", "G"]:
@@ -207,33 +213,46 @@ class GeneAminoAcidChangeToDNAVariants():
         return names
 
     def _process_coding_mutation(self, gene, ref, start, alt):
-        logger.debug("Processing gene:{} ref:{} start:{} alt:{}".format(
-            gene, ref, start, alt))
+        logger.debug(
+            "Processing gene:{} ref:{} start:{} alt:{}".format(gene, ref, start, alt)
+        )
         if not gene.prot or start > len(gene.prot):
-            raise ValueError("Error translating %s_%s " %
-                             (gene, "".join([ref, str(start), alt])))
+            raise ValueError(
+                "Error translating %s_%s " % (gene, "".join([ref, str(start), alt]))
+            )
         if not gene.prot[start - 1] == ref:
             raise ValueError(
-                "Error processing %s_%s. The reference at pos %i is not %s, it's %s. " %
-                (gene, "".join(
-                    [
-                        ref, str(start), alt]), start, ref, gene.prot[
-                    start - 1]))
+                "Error processing %s_%s. The reference at pos %i is not %s, it's %s. "
+                % (
+                    gene,
+                    "".join([ref, str(start), alt]),
+                    start,
+                    ref,
+                    gene.prot[start - 1],
+                )
+            )
         ref_codons = gene.get_reference_codons(start)
         alt_codons = self.get_reference_alts(gene, alt)
-        logger.debug("Reference codons (forward strand equivalent) {}".format(
-            "".join(ref_codons)))
-        logger.debug("Alternate codons (forward strand equivalent) {}".format(
-            "".join(alt_codons)))
+        logger.debug(
+            "Reference codons (forward strand equivalent) {}".format(
+                "".join(ref_codons)
+            )
+        )
+        logger.debug(
+            "Alternate codons (forward strand equivalent) {}".format(
+                "".join(alt_codons)
+            )
+        )
         for ref_codon in ref_codons:
             if ref_codon in alt_codons:
                 alt_codons.remove(ref_codon)
         location = self.get_location(gene, start)
         alternative = "/".join(alt_codons)
         ref_codon = gene.get_reference_codon(start)
-        names = ["".join(["".join(ref_codon),
-                          str(location),
-                          "".join(alt_codon)]) for alt_codon in alt_codons]
+        names = [
+            "".join(["".join(ref_codon), str(location), "".join(alt_codon)])
+            for alt_codon in alt_codons
+        ]
         return names
 
     def get_gene(self, gene):
