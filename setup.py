51abd802416250a5a8074fbd7ab7f76bf9cd181d
Martin Hunt <martin.g.hunt@gmail.com>
2022-04-22 10:56:56 +0100

    Version bump 0.11.0

--- setup.py
+++ setup.py
@@ -97,7 +97,7 @@ def read(*names, **kwargs):
 
 setup(
     name="mykrobe",
-    version="0.10.0",
+    version="0.11.0",
     license="MIT",
     description="Antibiotic resistance prediction in minutes",
     author="Phelim Bradley",
